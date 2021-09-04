edc6f884518a49f9d9c57d9a30610996df33292a
MihaiTSandu <stmihai98@gmail.com>
2021-09-04 20:03:37 +0300

    ci: add .env file

--- packages/client/src/utils/get-city-weather.ts
+++ packages/client/src/utils/get-city-weather.ts
@@ -1,6 +1,6 @@
 import { OpenWeatherResponse } from 'utils/open-weather-response';
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const OPEN_WEATHER_MAP_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 const getCityWeather = async (
   latitude?: string,
